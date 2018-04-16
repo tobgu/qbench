a75b355225ac63967d51bdacc2b6142e996d076b
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2018-04-16 13:01:57 +0200

    Benchmark writing JSON records

--- qframe_bench_test.go
+++ qframe_bench_test.go
@@ -1,6 +1,7 @@
 package qframe_test
 
 import (
+	"bytes"
 	df "github.com/kniren/gota/dataframe"
 	qf "github.com/tobgu/qframe"
 	"os"
@@ -40,6 +41,23 @@ func BenchmarkQFrame_ReadCsv(b *testing.B) {
 	}
 }
 
+func BenchmarkQFrame_WriteJsonRecords(b *testing.B) {
+	b.ReportAllocs()
+	f, _ := qframeReadCsv()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buf := new(bytes.Buffer)
+		f.ToJson(buf, "records")
+		if f.Err != nil {
+			b.Fatalf("Unexpected JSON error: %s", f.Err)
+		}
+
+		if buf.Len() != 33363821 {
+			b.Fatalf("Unexpected JSON length: %d", buf.Len())
+		}
+	}
+}
+
 func gotaReadCsv() (df.DataFrame, error) {
 	f, err := os.Open(dataFileName)
 	if err != nil {
@@ -70,7 +88,26 @@ func BenchmarkGota_ReadCSV(b *testing.B) {
 	}
 }
 
+func BenchmarkGota_WriteJsonRecords(b *testing.B) {
+	b.ReportAllocs()
+	f, _ := gotaReadCsv()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		buf := new(bytes.Buffer)
+		f.WriteJSON(buf)
+		if f.Err != nil {
+			b.Fatalf("Unexpected JSON error: %s", f.Err)
+		}
+
+		if buf.Len() != 33409725 {
+			b.Fatalf("Unexpected JSON length: %d", buf.Len())
+		}
+	}
+}
+
 /*
-BenchmarkQFrame_ReadCsv-2   	       5	 204340010 ns/op	164317976 B/op	    1501 allocs/op
-BenchmarkGota_ReadCSV-2     	       2	 794546955 ns/op	228592016 B/op	 3686955 allocs/op
+BenchmarkQFrame_ReadCsv-2            	       5	 202199966 ns/op	164318040 B/op	    1500 allocs/op
+BenchmarkQFrame_WriteJsonRecords-2   	       5	 224061889 ns/op	69792524 B/op	      74 allocs/op
+BenchmarkGota_ReadCSV-2              	       2	 771431210 ns/op	228591928 B/op	 3686954 allocs/op
+BenchmarkGota_WriteJsonRecords-2     	       1	2298617520 ns/op	482345720 B/op	 5827950 allocs/op
 */
