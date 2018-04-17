377f3e27f711605f62f12fbeff7ec4be439521a3
Tobias Gustafsson <tobias.l.gustafsson@gmail.com>
2018-04-17 13:52:32 +0200

    Some more filter benchmarks

--- qframe_bench_test.go
+++ qframe_bench_test.go
@@ -12,6 +12,14 @@ const rowCount = 73861
 const columnCount = 23
 const dataFileName = "recipeData-utf8.csv"
 
+func intRange(size int) []int {
+	result := make([]int, size)
+	for i := range result {
+		result[i] = i
+	}
+	return result
+}
+
 func qframeReadCsv() (qf.QFrame, error) {
 	f, err := os.Open(dataFileName)
 	if err != nil {
@@ -89,7 +97,7 @@ func BenchmarkQFrame_Sort(b *testing.B) {
 	}
 }
 
-func F(comparator, column string, arg interface{}) qf.Filter {
+func F(comparator interface{}, column string, arg interface{}) qf.Filter {
 	return qf.Filter{Comparator: comparator, Column: column, Arg: arg}
 }
 
@@ -102,6 +110,7 @@ func BenchmarkQFrame_Filter(b *testing.B) {
 		expectedCount int
 	}{
 		{"Float gt", F(">", "Size(L)", 21.0), 26823},
+		{"Float custom gt", F(func(f float64) bool { return f > 21.0 }, "Size(L)", nil), 26823},
 		{"Combine or", qf.Or(F(">", "Size(L)", 21.0), F(">", "StyleID", 100)), 39818},
 		{"Combine and", qf.And(F(">", "Size(L)", 21.0), F(">", "StyleID", 100)), 7280},
 		{"String eq", F("=", "Style", "Cream Ale"), 830},
@@ -109,6 +118,7 @@ func BenchmarkQFrame_Filter(b *testing.B) {
 		{"String like case insensitive", F("ilike", "Name", "%ale%"), 11912},
 		{"String regex case sensitive", F("like", "Name", ".*Ale.*"), 9118},
 		{"String regex case insensitive", F("ilike", "Name", ".*ale.*"), 11912},
+		{"Integer in", F("in", "StyleID", intRange(100)), 53514},
 	}
 
 	for _, bc := range benchmarks {
@@ -234,6 +244,12 @@ func BenchmarkGota_Filter(b *testing.B) {
 				return f.Filter(df.F{Colname: "Style", Comparator: "==", Comparando: "Cream Ale"})
 			},
 			expectedCount: 830},
+		{
+			name: "Integer in",
+			filter: func(f df.DataFrame) df.DataFrame {
+				return f.Filter(df.F{Colname: "StyleID", Comparator: "in", Comparando: intRange(100)})
+			},
+			expectedCount: 53514},
 	}
 
 	for _, bc := range benchmarks {
@@ -262,6 +278,7 @@ BenchmarkQFrame_Sort/UserId_-_Int-2  	     100	  10306055 ns/op	  303152 B/op	  
 BenchmarkQFrame_Sort/Name_-__string-2         	      20	  86066995 ns/op	  303184 B/op	       3 allocs/op
 BenchmarkQFrame_Sort/Multi_column-2           	      10	 156379617 ns/op	  303344 B/op	       5 allocs/op
 BenchmarkQFrame_Filter/Float_gt-2             	    2000	    925995 ns/op	  196608 B/op	       2 allocs/op
+BenchmarkQFrame_Filter/Float_custom_gt-2    	    2000	   1130335 ns/op	  196608 B/op	       2 allocs/op
 BenchmarkQFrame_Filter/Combine_or-2           	    1000	   1352508 ns/op	  245936 B/op	       4 allocs/op
 BenchmarkQFrame_Filter/Combine_and-2          	    1000	   1197479 ns/op	  256800 B/op	       6 allocs/op
 BenchmarkQFrame_Filter/String_eq-2            	    2000	   1076656 ns/op	   85376 B/op	       2 allocs/op
@@ -269,6 +286,7 @@ BenchmarkQFrame_Filter/String_like_case_sensitive-2         	     500	   3777651
 BenchmarkQFrame_Filter/String_like_case_insensitive-2       	     100	  14342672 ns/op	  131704 B/op	      16 allocs/op
 BenchmarkQFrame_Filter/String_regex_case_sensitive-2        	      20	  67469026 ns/op	  164712 B/op	      58 allocs/op
 BenchmarkQFrame_Filter/String_regex_case_insensitive-2      	      20	  88342431 ns/op	  172972 B/op	      61 allocs/op
+BenchmarkQFrame_Filter/Integer_in-2                         	     500	   3179964 ns/op	  304811 B/op	      10 allocs/op
 
 BenchmarkGota_ReadCSV-2                                     	       2	 758721612 ns/op	228591928 B/op	 3686954 allocs/op
 BenchmarkGota_WriteJsonRecords-2                            	       1	2771840823 ns/op	482439320 B/op	 5828275 allocs/op
@@ -279,4 +297,5 @@ BenchmarkGota_Filter/Float_gt-2                             	      50	  32328655
 BenchmarkGota_Filter/Combine_or-2                           	      20	  51720372 ns/op	60522417 B/op	     663 allocs/op
 BenchmarkGota_Filter/Combine_and-2                          	      30	  42981669 ns/op	48312308 B/op	    1103 allocs/op
 BenchmarkGota_Filter/String_eq-2                            	     200	   9020487 ns/op	 1087112 B/op	     310 allocs/op
+BenchmarkGota_Filter/Integer_in-2                           	      10	 189430304 ns/op	77769508 B/op	     779 allocs/op
 */
--- test_pandas_bench.py
+++ test_pandas_bench.py
@@ -1,5 +1,6 @@
 # - Group by aggregate
 # - Apply
+# - Filter with custom function
 
 import pandas as pd
 import pytest
@@ -49,23 +50,24 @@ def test_sort(benchmark, columns):
     ("contains case insensitive", lambda df: df[df["Name"].str.contains("ale", case=False, na=False)], 11912),
     ("regex case sensitive", lambda df: df[df["Name"].str.contains(".*Ale.*", case=True, na=False)], 9118),
     ("regex case insensitive", lambda df: df[df["Name"].str.contains(".*ale.*", case=False, na=False)], 11912),
+    ("integer in", lambda df: df[df["StyleID"].isin([i for i in range(100)])], 53514),
 ])
 def test_filter(benchmark, name, filter_fn, expected_count):
     df = read_csv()
     new_df = benchmark(filter_fn, df)
     assert len(new_df) == expected_count
 
-
 # Name (time in ms)                                             Mean              Median            StdDev            Rounds
 # --------------------------------------------------------------------------------------------------------------------------
-# test_filter[combine and-<lambda>-7280]                      5.0941 (1.0)        4.9476 (1.0)      0.7620 (1.66)        175
-# test_filter[combine or-<lambda>-39818]                     10.0231 (1.97)       9.9354 (2.01)     0.4589 (1.0)         102
-# test_filter[contains case insensitive-<lambda>-11912]      98.2903 (19.29)     98.7691 (19.96)    2.2099 (4.82)         10
-# test_filter[contains case sensitive-<lambda>-9118]         59.1438 (11.61)     58.6890 (11.86)    3.4401 (7.50)         18
-# test_filter[regex case insensitive-<lambda>-11912]        305.6966 (60.01)    306.0521 (61.86)    4.8669 (10.61)         5
-# test_filter[regex case sensitive-<lambda>-9118]           128.5158 (25.23)    127.9879 (25.87)    3.7973 (8.27)          9
-# test_filter[single float-<lambda>-26823]                    7.4126 (1.46)       7.2683 (1.47)     0.7895 (1.72)        135
-# test_filter[string eq-<lambda>-830]                        10.0484 (1.97)       9.8872 (2.00)     0.9117 (1.99)         93
+# test_filter[combine and-<lambda>-7280]                      4.6996 (1.0)        4.5539 (1.0)      0.4474 (1.0)         192
+# test_filter[combine or-<lambda>-39818]                     10.2313 (2.18)      10.0739 (2.21)     0.6902 (1.54)         89
+# test_filter[contains case insensitive-<lambda>-11912]     100.4382 (21.37)     99.3378 (21.81)    3.4898 (7.80)         11
+# test_filter[contains case sensitive-<lambda>-9118]         59.0800 (12.57)     58.2704 (12.80)    2.0572 (4.60)         18
+# test_filter[integer in-<lambda>-53514]                     11.6831 (2.49)      11.4531 (2.51)     0.9151 (2.05)         85
+# test_filter[regex case insensitive-<lambda>-11912]        304.9066 (64.88)    306.2898 (67.26)    2.9705 (6.64)          5
+# test_filter[regex case sensitive-<lambda>-9118]           126.5499 (26.93)    125.9932 (27.67)    3.2897 (7.35)          8
+# test_filter[single float-<lambda>-26823]                    7.3997 (1.57)       7.1886 (1.58)     0.8638 (1.93)        113
+# test_filter[string eq-<lambda>-830]                        10.1308 (2.16)       9.9458 (2.18)     1.0313 (2.31)         92
 # test_read_csv                                             416.8440 (81.83)    416.2372 (84.13)    2.7442 (5.98)          5
 # test_sort[columns0]                                        22.8061 (4.48)      22.5379 (4.56)     1.0598 (2.31)         42
 # test_sort[columns1]                                       142.2413 (27.92)    143.0750 (28.92)    2.4375 (5.31)          8
